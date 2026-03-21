b80d6e1927da7c5ee02b78ee4a855c9b544bf1cf
Matthias <157816473+koma6379@users.noreply.github.com>
2026-03-21 10:24:46 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * KONFIGURATION
+ * Bitte fülle die folgenden Felder mit deinen tatsächlichen 
+ * Datenbank-Zugangsdaten und deinem gewünschten VIP-Code aus.
+ */
+
+$host    = 'localhost';         // Meistens 'localhost'
+$db      = 'DEIN_DB_NAME';      // Name deiner Datenbank
+$user    = 'DEIN_NUTZERNAME';   // Benutzername für die Datenbank
+$pass    = 'DEIN_PASSWORT';     // Passwort für die Datenbank
+$vipCode = 'DEIN_VIP_CODE';     // Der Code, den Gäste eingeben müssen
+?>
